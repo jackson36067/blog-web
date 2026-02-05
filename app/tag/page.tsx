bd9759febf9bc26d816f80f837e476b26f76a795
jackson36067 <jacksonn36067@gmail.com>
2026-02-05 22:07:19 +0800

    bug: tag component return bug

--- app/tag/page.tsx
+++ app/tag/page.tsx
@@ -4,7 +4,9 @@ import TagComponentPage from "@/components/tag/TagPageComponent";
 import { Suspense } from "react";
 
 export default function TagPage() {
-  <Suspense>
-    <TagComponentPage></TagComponentPage>
-  </Suspense>;
+  return (
+    <Suspense>
+      <TagComponentPage></TagComponentPage>
+    </Suspense>
+  );
 }
--- utils/http.ts
+++ utils/http.ts
@@ -1,47 +1,47 @@
-import axios from 'axios'
-import { toast } from 'sonner'
-import useMemberStore from '@/stores/UserStore'
-import qs from 'qs'
+import axios from "axios";
+import { toast } from "sonner";
+import useMemberStore from "@/stores/UserStore";
+import qs from "qs";
 
 const httpInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: "http://localhost:8080/api",
   timeout: 5000,
   // 这里全局设置 params 序列化方式 (去除传递param方式的参数中数组参数自动带上[])
-  paramsSerializer: params => qs.stringify(params, { indices: false }),
-})
+  paramsSerializer: (params) => qs.stringify(params, { indices: false }),
+});
 
 // 请求拦截
 httpInstance.interceptors.request.use(
-  config => {
-    const urls = ['/login']
+  (config) => {
+    const urls = ["/login"];
     if (!urls.includes(config.url!)) {
-      const token: string = useMemberStore.getState().userInfo.token || ''
+      const token: string = useMemberStore.getState().userInfo.token || "";
       if (token) {
-        config.headers.Authorization = `Bearer ${token}`
+        config.headers.Authorization = `Bearer ${token}`;
       }
     }
-    return config
+    return config;
   },
-  error => {
-    return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   },
-)
+);
 
 // 响应拦截
 httpInstance.interceptors.response.use(
-  config => {
-    if (config.data.message) toast.success(config.data.message)
-    return config.data
+  (config) => {
+    if (config.data.message) toast.success(config.data.message);
+    return config.data;
   },
-  error => {
+  (error) => {
     if (error.response.data.code === 401 || error.response.data.code === 403) {
-      useMemberStore.getState().clearUserInfo()
+      useMemberStore.getState().clearUserInfo();
     }
-    const errorMsg: string = error.response.data.message || '请求失败'
-    console.error('HTTP Error:', error)
-    toast.error(errorMsg)
-    return Promise.reject(error)
+    const errorMsg: string = error.response.data.message || "请求失败";
+    console.error("HTTP Error:", error);
+    toast.error(errorMsg);
+    return Promise.reject(error);
   },
-)
+);
 
-export default httpInstance
+export default httpInstance;
